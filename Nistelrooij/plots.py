6c9b6023d8eb95a0ef398bee788cbbbdfa987118
ovacia345 <nielsvann@hotmail.nl>
2019-03-20 23:47:24 +0100

    Add plots that show the updates of each parameter's expected value

--- Nistelrooij/plots.py
+++ Nistelrooij/plots.py
@@ -10,42 +10,84 @@ def plotProbTable(rods, prob_table):
 
 
 class StimuliPlotter:
-    def __init__(self, iterations_num, stim_min, stim_max):
+    def __init__(self, iterations_num, stim_min, stim_max, params_gen):
+        # initialize members
         self.iterations_num = iterations_num
         self.stim_min = stim_min * 180 / np.pi
         self.stim_max = stim_max * 180 / np.pi
+        self.params_gen = params_gen
 
+        # size of points in parameter values graph
+        self.point_size = 2
+
+        # initialize the plots
         plt.ion()
-        figure = plt.figure()
-        self.plot = figure.add_subplot(1, 1, 1)
+        figure = plt.figure(figsize=(15, 10))
+        self.plot_stimuli = figure.add_subplot(2, 3, 1)
+        self.plot_kappa_ver = figure.add_subplot(2, 3, 2)
+        self.plot_kappa_hor = figure.add_subplot(2, 3, 3)
+        self.plot_tau = figure.add_subplot(2, 3, 4)
+        self.plot_kappa_oto = figure.add_subplot(2, 3, 5)
+        self.plot_lapse = figure.add_subplot(2, 3, 6)
 
+        # initialize stimuli
         self.stimuli = ([], [])
 
+        # initialize parameter and parameter plots dictionaries
+        self.params = {'kappa_ver': [], 'kappa_hor': [], 'tau': [], 'kappa_oto': [], 'lapse': []}
+        self.param_plots = {'kappa_ver': self.plot_kappa_ver, 'kappa_hor': self.plot_kappa_hor, 'tau': self.plot_tau,
+                            'kappa_oto': self.plot_kappa_oto, 'lapse': self.plot_lapse}
+
     def plotStimuli(self, rod, frame):
+        # add rod and frame to self.stimuli
         self.stimuli[0].append(rod * 180.0 / np.pi)
         self.stimuli[1].append(frame * 180.0 / np.pi)
+
+        # compute current trial number
         trial_num = len(self.stimuli[0])
 
-        self.plot.clear()
-        self.plot.plot(np.arange(trial_num), self.stimuli[0], 'b.', label='rod [deg]')
-        self.plot.plot(np.arange(trial_num), self.stimuli[1], 'r.', label='frame [deg]')
-        self.plot.set_xlabel('trial')
-        self.plot.set_ylabel('selected stimulus [deg]')
-        self.plot.set_xlim(0, self.iterations_num)
-        self.plot.set_ylim(self.stim_min, self.stim_max)
-        self.plot.set_title('Selected Stimuli')
-        self.plot.legend()
+        # plot selected stimuli
+        self.plot_stimuli.clear()
+        self.plot_stimuli.scatter(np.arange(trial_num), self.stimuli[0], s=self.point_size, label='rod [deg]')
+        self.plot_stimuli.scatter(np.arange(trial_num), self.stimuli[1], s=self.point_size, label='frame [deg]')
+        self.plot_stimuli.set_xlabel('trial number')
+        self.plot_stimuli.set_ylabel('selected stimulus [deg]')
+        self.plot_stimuli.set_xlim(0, self.iterations_num)
+        self.plot_stimuli.set_ylim(self.stim_min, self.stim_max)
+        self.plot_stimuli.set_title('Selected Stimuli for each Trial')
+        self.plot_stimuli.legend()
 
+        # pause to let pyplot draw plot
         plt.pause(0.0001)
 
+    def plotParameterValues(self, params):
+        # draw each parameter values plot
+        for param in self.params.keys():
+            # add parameter value to self.params
+            self.params[param].append(params[param])
 
+            # plot specific parameter values graph
+            self.__plotParemeterValues(param)
 
-def plotP(rods, P, title):
-    plt.figure()
-    plt.plot(rods * 180 / np.pi, P)
-    plt.title(title)
-    plt.xlabel('rod [deg]')
-    plt.ylabel('P(right)')
+        # pause to let pyplot draw graphs
+        plt.pause(0.0001)
 
-def show():
-    plt.show()
+        # fit all the plots to the screen with no overlapping text
+        plt.tight_layout()
+
+    def __plotParemeterValues(self, param):
+        # compute current trial number
+        trial_num = len(self.params[param])
+
+        # retreive specific parameter plot from self.param_plots
+        plot = self.param_plots[param]
+
+        # plot specific parameter values
+        plot.clear()
+        plot.plot([1, self.iterations_num], [self.params_gen[param]]*2, c='r')
+        plot.scatter(np.arange(trial_num), self.params[param], s=self.point_size)
+        plot.set_xlabel('trial number')
+        plot.set_ylabel(param)
+        plot.set_xlim(0, self.iterations_num)
+        plot.set_title('Calculated %s Values for each Trial' % param)
+        plot.legend()
--- Nistelrooij/test.py
+++ Nistelrooij/test.py
@@ -13,10 +13,10 @@ def sig2kap(sig): #in degrees
     return 3.9945e3/(sig2+0.0226e3)
 
 
-# kappa_ver = np.linspace(sig2kap(2.3), sig2kap(7.4), 6)
-kappa_ver = [sig2kap(4.9)]
-# kappa_hor = np.linspace(sig2kap(28), sig2kap(76), 6)
-kappa_hor = [sig2kap(52.0)]
+kappa_ver = np.linspace(sig2kap(2.3), sig2kap(7.4), 6)
+# kappa_ver = [sig2kap(4.9)]
+kappa_hor = np.linspace(sig2kap(28), sig2kap(76), 6)
+# kappa_hor = [sig2kap(52.0)]
 tau = np.linspace(0.6, 1.0, 6)
 # tau = [0.8]
 kappa_oto = np.linspace(sig2kap(1.4), sig2kap(3.0), 6)
@@ -28,7 +28,7 @@ rods = np.array([-7, -4, -2, -1, 0, 1, 2, 4, 7]) * np.pi / 180
 frames = np.linspace(-45, 40, 18) * np.pi / 180
 
 
-kappa_ver_gen = sig2kap(12) # 4.9
+kappa_ver_gen = sig2kap(4.9)
 kappa_hor_gen = sig2kap(52)
 tau_gen = 0.8
 kappa_oto_gen = sig2kap(2.2)
@@ -40,7 +40,7 @@ params_gen = {'kappa_ver': kappa_ver_gen,
               'kappa_oto': kappa_oto_gen,
               'lapse': lapse_gen}
 
-
+# initialize generative agent and show rod distribution plot for each frame orientation
 genAgent = GenerativeAgent(kappa_ver_gen, kappa_hor_gen, tau_gen, kappa_oto_gen, lapse_gen, rods, frames)
 plots.plotProbTable(rods, genAgent.prob_table)
 
@@ -51,7 +51,7 @@ for stim_selection in ['adaptive', 'random']:
     psi = PSI_RiF(kappa_ver, kappa_hor, tau, kappa_oto, lapse, rods, frames, stim_selection)
 
     # initialize stimuli plotter object
-    plotter = StimuliPlotter(iterations_num, np.amin(frames), np.amax(frames))
+    plotter = StimuliPlotter(iterations_num, np.amin(frames), np.amax(frames), params_gen)
 
     # run model for given number of iterations
     print 'inferring model'
@@ -61,8 +61,11 @@ for stim_selection in ['adaptive', 'random']:
         # get stimulus from psi object
         rod, frame = psi.stim
 
-        # plot chosen stimuli
+        # plot selected stimuli
         plotter.plotStimuli(rod, frame)
+
+        # plot updated parameter values based on mean
+        plotter.plotParameterValues(psi.calcParameterValues('mean'))
 
         # get response from the generative model
         response = genAgent.getResponse(rod, frame)
