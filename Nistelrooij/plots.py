537e165a8164ee49db162c22cce6115a31ad4d21
ovacia345 <nielsvann@hotmail.nl>
2019-03-21 23:25:33 +0100

    Add figure that shows parameter values distribution for all parameters for the current trial

--- Nistelrooij/plots.py
+++ Nistelrooij/plots.py
@@ -3,27 +3,28 @@ import matplotlib.pyplot as plt
 
 def plotProbTable(rods, prob_table):
     plt.plot(rods * 180 / np.pi, prob_table)
-    plt.title('Generative Rod Distribution for each Frame Orientation')
+    plt.title('Generative Rod Distribution for Each Frame Orientation')
     plt.xlabel('rod [deg]')
     plt.ylabel('P(right)')
     plt.pause(0.001)
 
 
-class StimuliPlotter:
+class Plotter:
 
-    def __init__(self, iterations_num, stim_min, stim_max, params_gen):
+    def __init__(self, iterations_num, params, params_gen):
         self.iterations_num = iterations_num
 
-        # set minimal and maximal stimulus values in degrees
-        self.stim_min = stim_min * 180 / np.pi
-        self.stim_max = stim_max * 180 / np.pi
+        # set the parameter values and the shape when combining the parameter values
+        self.params = params
+        self.params_shape = [len(self.params[param]) for param in ['kappa_ver', 'kappa_hor', 'tau', 'kappa_oto', 'lapse']]
 
         # set the generative parameter values
         self.params_gen = params_gen
 
         # initialize stimuli and params as Nones
         self.stimuli = None
-        self.params = None
+        self.param_values = None
+        self.param_distributions = None
 
 
     def initStimuliFigure(self):
@@ -35,11 +36,12 @@ class StimuliPlotter:
         self.stimuli = ([], [])
 
 
-    def plotStimuli(self, rod, frame):
+    def plotStimuli(self, psi):
         if self.stimuli is None:
             self.initStimuliFigure()
 
-        # add rod and frame to self.stimuli
+        # add stimuli to self.stimuli as degrees
+        rod, frame = psi.stim
         self.stimuli[0].append(rod * 180.0 / np.pi)
         self.stimuli[1].append(frame * 180.0 / np.pi)
 
@@ -53,47 +55,51 @@ class StimuliPlotter:
         self.plot_stimuli.set_xlabel('trial number')
         self.plot_stimuli.set_ylabel('selected stimulus [deg]')
         self.plot_stimuli.set_xlim(0, self.iterations_num)
-        self.plot_stimuli.set_ylim(self.stim_min, self.stim_max)
-        self.plot_stimuli.set_title('Selected Stimuli for each Trial')
+        self.plot_stimuli.set_title('Selected Stimuli for Each Trial')
         self.plot_stimuli.legend()
 
         # pause to let pyplot draw plot
         plt.pause(0.0001)
 
 
-    def initParameterFigure(self):
+    def initParameterValuesFigure(self):
         # initialize calculated parameter values plots
-        figure_params = plt.figure(figsize=(15, 8))
-        self.plot_kappa_ver = figure_params.add_subplot(2, 3, 1)
-        self.plot_kappa_hor = figure_params.add_subplot(2, 3, 2)
-        self.plot_tau = figure_params.add_subplot(2, 3, 4)
-        self.plot_kappa_oto = figure_params.add_subplot(2, 3, 5)
-        self.plot_lapse = figure_params.add_subplot(2, 3, 6)
+        self.figure_param_values = plt.figure(figsize=(15, 8))
+        plot_kappa_ver = self.figure_param_values.add_subplot(2, 3, 1)
+        plot_kappa_hor = self.figure_param_values.add_subplot(2, 3, 2)
+        plot_tau = self.figure_param_values.add_subplot(2, 3, 4)
+        plot_kappa_oto = self.figure_param_values.add_subplot(2, 3, 5)
+        plot_lapse = self.figure_param_values.add_subplot(2, 3, 6)
 
         # size of points in parameter values graph
         self.point_size = 5
 
-        # initialize parameter and parameter plot dictionaries
-        self.params = {'kappa_ver': [], 'kappa_hor': [], 'tau': [], 'kappa_oto': [], 'lapse': []}
-        self.param_plots = {'kappa_ver': self.plot_kappa_ver, 'kappa_hor': self.plot_kappa_hor, 'tau': self.plot_tau,
-                            'kappa_oto': self.plot_kappa_oto, 'lapse': self.plot_lapse}
+        # initialize parameter values and parameter plots dictionaries
+        self.param_values = {'MAP': {'kappa_ver': [], 'kappa_hor': [], 'tau': [], 'kappa_oto': [], 'lapse': []},
+                             'mean': {'kappa_ver': [], 'kappa_hor': [], 'tau': [], 'kappa_oto': [], 'lapse': []}}
+        self.param_values_plots = {'kappa_ver': plot_kappa_ver, 'kappa_hor': plot_kappa_hor, 'tau': plot_tau,
+                            'kappa_oto': plot_kappa_oto, 'lapse': plot_lapse}
 
 
-    def plotParameterValues(self, params):
-        if self.params is None:
-            self.initParameterFigure()
+    def plotParameterValues(self, psi):
+        if self.param_values is None:
+            self.initParameterValuesFigure()
+
+        param_values_MAP = psi.calcParameterValues('MAP')
+        param_values_mean = psi.calcParameterValues('mean')
 
         # draw each parameter's values plot
         for param in self.params.keys():
-            # add parameter value to self.params
-            self.params[param].append(params[param])
+            # add parameter value to self.param_values
+            self.param_values['MAP'][param].append(param_values_MAP[param])
+            self.param_values['mean'][param].append(param_values_mean[param])
 
             # plot specific parameter's values graph
             self.__plotParemeterValues(param)
 
         # add a single legend to the figure
-        handles, labels = self.plot_kappa_ver.get_legend_handles_labels()
-        self.plot_kappa_ver.get_figure().legend(handles, labels, loc='upper right')
+        handles, labels = self.param_values_plots['kappa_ver'].get_legend_handles_labels()
+        self.figure_param_values.legend(handles, labels, loc='upper right')
 
         # fit all the plots to the screen with no overlapping text
         plt.tight_layout()
@@ -104,16 +110,73 @@ class StimuliPlotter:
 
     def __plotParemeterValues(self, param):
         # compute current trial number
-        trial_num = len(self.params[param])
+        trial_num = len(self.param_values['MAP'][param])
 
-        # retreive specific parameter plot from self.param_plots
-        plot = self.param_plots[param]
+        # retrieve specific parameter plot from self.param_values_plots
+        plot = self.param_values_plots[param]
 
         # plot specific parameter values
         plot.clear()
         plot.hlines(self.params_gen[param], 1, self.iterations_num, label='generative parameter value')
-        plot.scatter(np.arange(trial_num), self.params[param], s=self.point_size, label='calculated parameter value')
+        plot.scatter(np.arange(trial_num), self.param_values['MAP'][param], s=self.point_size, label='parameter value based on MAP')
+        plot.scatter(np.arange(trial_num), self.param_values['mean'][param], s=self.point_size, label='expected parameter value')
         plot.set_xlabel('trial number')
         plot.set_ylabel(param)
         plot.set_xlim(0, self.iterations_num)
-        plot.set_title('Calculated %s Values for each Trial' % param)
+        plot.set_title('Calculated %s Values for Each Trial' % param)
+
+
+    def initParemeterDistributionsFigure(self):
+        # initialize calculated parameter values plots
+        self.figure_param_distributions = plt.figure(figsize=(15, 8))
+        plot_kappa_ver = self.figure_param_distributions.add_subplot(2, 3, 1)
+        plot_kappa_hor = self.figure_param_distributions.add_subplot(2, 3, 2)
+        plot_tau = self.figure_param_distributions.add_subplot(2, 3, 4)
+        plot_kappa_oto = self.figure_param_distributions.add_subplot(2, 3, 5)
+        plot_lapse = self.figure_param_distributions.add_subplot(2, 3, 6)
+
+        # initialize parameter plot dictionary
+        self.param_distributions_plots = {'kappa_ver': plot_kappa_ver, 'kappa_hor': plot_kappa_hor, 'tau': plot_tau,
+                            'kappa_oto': plot_kappa_oto, 'lapse': plot_lapse}
+
+
+    def plotParameterDistributions(self, psi):
+        if self.param_distributions is None:
+            self.initParemeterDistributionsFigure()
+
+        # get posterior from psi object in right shape
+        posterior = psi.prior.reshape(self.params_shape)
+
+        # compute the distributions of the parameters
+        kappa_ver = posterior.sum(4).sum(3).sum(2).sum(1)
+        kappa_hor = posterior.sum(4).sum(3).sum(2).sum(0)
+        tau = posterior.sum(4).sum(3).sum(1).sum(0)
+        kappa_oto = posterior.sum(4).sum(2).sum(1).sum(0)
+        lapse = posterior.sum(3).sum(2).sum(1).sum(0)
+
+        # put distributions in dictionary
+        self.param_distributions = {'kappa_ver': kappa_ver, 'kappa_hor': kappa_hor, 'tau': tau, 'kappa_oto': kappa_oto,
+                                    'lapse': lapse}
+
+        # plot the plot for each parameter
+        for param in self.params.keys():
+            # retrieve specific parameter plot from self.param_distributions_plots
+            plot = self.param_distributions_plots[param]
+
+            # plot specific parameter distribution
+            plot.clear()
+            plot.vlines(self.params_gen[param], 0, 1, label='generative parameter value')
+            plot.plot(self.params[param], self.param_distributions[param], label='parameter value distribution')
+            plot.set_xlabel(param)
+            plot.set_ylabel('probability')
+            plot.set_title('%s Distribution for Current Trial' % param)
+
+        # add a single legend to the figure
+        handles, labels = self.param_distributions_plots['kappa_ver'].get_legend_handles_labels()
+        self.figure_param_distributions.legend(handles, labels, loc='upper right')
+
+        # fit all the plots to the screen with no overlapping text
+        plt.tight_layout()
+
+        # pause to let pyplot draw graphs
+        plt.pause(0.0001)
--- Nistelrooij/test.py
+++ Nistelrooij/test.py
@@ -3,8 +3,7 @@ from tqdm import trange
 
 from GenerativeAgent import GenerativeAgent
 from PSI_RiF import PSI_RiF
-import plots
-from plots import StimuliPlotter
+from plots import *
 
 
 # transforms sigma values into kappa values
@@ -13,23 +12,30 @@ def sig2kap(sig): #in degrees
     return 3.9945e3/(sig2+0.0226e3)
 
 
-kappa_ver = np.linspace(sig2kap(2.3), sig2kap(7.4), 4)
+kappa_ver = np.linspace(sig2kap(2.3), sig2kap(7.4), 10)
 # kappa_ver = [sig2kap(4.9)]
-kappa_hor = np.linspace(sig2kap(28), sig2kap(76), 4)
+kappa_hor = np.linspace(sig2kap(28), sig2kap(76), 10)
 # kappa_hor = [sig2kap(52.0)]
-tau = np.linspace(0.6, 1.0, 4)
+tau = np.linspace(0.6, 1.0, 10)
 # tau = [0.8]
-kappa_oto = np.linspace(sig2kap(1.4), sig2kap(3.0), 4)
+kappa_oto = np.linspace(sig2kap(1.4), sig2kap(3.0), 10)
 # kappa_oto = [sig2kap(2.2)]
-lapse = np.linspace(0.0, 0.1, 4)
-# lapse = [0.0]
+lapse = np.linspace(0.0, 0.1, 10)
+# lapse = [0.02]
+
+params = {'kappa_ver': kappa_ver,
+          'kappa_hor': kappa_hor,
+          'tau': tau,
+          'kappa_oto': kappa_oto,
+          'lapse': lapse}
+
 
 rods = np.array([-7, -4, -2, -1, 0, 1, 2, 4, 7]) * np.pi / 180
 frames = np.linspace(-45, 40, 18) * np.pi / 180
 
 
 kappa_ver_gen = sig2kap(4.9)
-kappa_hor_gen = sig2kap(52)
+kappa_hor_gen = sig2kap(52.0)
 tau_gen = 0.8
 kappa_oto_gen = sig2kap(2.2)
 lapse_gen = 0.02
@@ -42,7 +48,7 @@ params_gen = {'kappa_ver': kappa_ver_gen,
 
 # initialize generative agent and show rod distribution plot for each frame orientation
 genAgent = GenerativeAgent(kappa_ver_gen, kappa_hor_gen, tau_gen, kappa_oto_gen, lapse_gen, rods, frames)
-plots.plotProbTable(rods, genAgent.prob_table)
+plotProbTable(rods, genAgent.prob_table)
 
 # test for adaptive and random stimulus selection
 iterations_num = 500
@@ -50,8 +56,8 @@ for stim_selection in ['adaptive', 'random']:
     # initialize psi object
     psi = PSI_RiF(kappa_ver, kappa_hor, tau, kappa_oto, lapse, rods, frames, stim_selection)
 
-    # initialize stimuli plotter object
-    plotter = StimuliPlotter(iterations_num, np.amin(frames), np.amax(frames), params_gen)
+    # initialize plotter object
+    plotter = Plotter(iterations_num, params, params_gen)
 
     # run model for given number of iterations
     print 'inferring model'
@@ -62,10 +68,13 @@ for stim_selection in ['adaptive', 'random']:
         rod, frame = psi.stim
 
         # plot selected stimuli
-        plotter.plotStimuli(rod, frame)
+        plotter.plotStimuli(psi)
 
         # plot updated parameter values based on mean
-        plotter.plotParameterValues(psi.calcParameterValues('mean'))
+        plotter.plotParameterValues(psi)
+
+        # plot parameter distributions of current trial
+        plotter.plotParameterDistributions(psi)
 
         # get response from the generative model
         response = genAgent.getResponse(rod, frame)
